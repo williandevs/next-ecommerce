eb0c7d698a90ce2a2e76709e5b7da22c65769af8
Willian Figueiredo <williamdev38@gmail.com>
2025-08-31 14:25:10 -0300

    first commit

---
+++ src/app/product/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const Product = () => {
+  return (
+    <div>
+      Product
+      <Button className="p-3 text-orange-500 w-full rounded-full bg-red-900">
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default Product;
